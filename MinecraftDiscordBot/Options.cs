188e1faeb53970edb78a041a55fd5a7bd63967bc
Gabriele Lopez <gabriele.ope@gmail.com>
2023-04-21 12:34:18 +0200

    Options class

---
+++ MinecraftDiscordBot/Options.cs
@@ -0,0 +1,14 @@
+﻿namespace MinecraftDiscordBot;
+
+public class Options
+{
+    public string Token { get; }
+    public string Location { get; }
+
+    public Options(string token, string location)
+    {
+        Token = token;
+        Location = location;
+    }
+}
+    
